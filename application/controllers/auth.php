bc11f377e266cedcadffe8472c302ac0000742fb
arifka-rizki <rifka.rizki.fakhriza@gmail.com>
2020-10-14 09:34:32 +0700

    login dan logout

---
+++ application/controllers/auth.php
@@ -0,0 +1,55 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class auth extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(array('form_validation', 'session'));
+        $this->load->model('m_login');
+    }
+
+	public function index()
+	{
+        $this->load->view('templates/header');
+        $this->load->view('login');
+        $this->load->view('templates/footer');
+    }
+    
+    public function login_action(){
+        $this->form_validation->set_rules('nik', 'NIK', 'required|numeric');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+
+        if($this->form_validation->run()==false){
+            $this->load->view('templates/header');
+            $this->load->view('login');
+            $this->load->view('templates/footer');
+        } else{
+            $nik = $this->input->post('nik');
+            $password = $this->input->post('password');
+            $where = array(
+                'nik' => $nik,
+                'password' => $password
+            );
+            $check = $this -> m_login->check_login("user", $where)->num_rows();
+            if($check > 0){
+                $data_session=array(
+                    'nik' => $nik,
+                    'status' => "login"
+                );
+
+                $this->session->set_userdata($data_session);
+
+                redirect(base_url("dashboard"));
+            } else{
+                echo "nik dan pass salah";
+            }
+        }
+    }
+
+    public function logout(){
+        $this->session->sess_destroy();
+        redirect(base_url("auth"));
+    }
+}
---
+++ application/views/login.php
@@ -0,0 +1,53 @@
+    <!-- Custom styles for this template -->
+  </head>
+
+  <body class="bg-light">
+    <div class="container">
+      <div class="row justify-content-center pt-5 mt-5">
+        <div class="col-md-6">
+          <form class="needs-validation border p-5 rounded bg-white shadow-lg" novalidate method="POST" action="<?php echo base_url('auth/login_action'); ?>">
+            <h4 class="mb-3"><b>Sign in</b></h4>
+
+            <div class="mb-3">
+              <label for="nik"> <b>NIK/NIP</b></label>
+              <input
+                type="text"
+                class="form-control"
+                id="nik"
+                placeholder=""
+                required
+                name="nik"
+              />
+              <div class="invalid-feedback">Tolong masukkan NIK.</div>
+            </div>
+
+            <div class="mb-3">
+              <label for="pass"><b>Password</b></label>
+              <input
+                type="password"
+                class="form-control"
+                id="pass"
+                placeholder=""
+                value=""
+                required
+                name="password"
+              />
+            </div>
+
+            <hr class="mb-4" />
+
+            <button class="btn btn-danger btn-lg btn-block" type="submit">
+              Sign in
+            </button>
+
+            <p class="text-center my-4">Atau</p>
+
+            <a
+              href="profil.html"
+              class="btn btn-outline-danger btn-lg btn-block"
+              >Peserta Eksternal</a
+            >
+          </form>
+        </div>
+      </div>
+    </div>
