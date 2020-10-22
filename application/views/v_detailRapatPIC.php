6b97c0311b625b4f11384b612ad694fd1095eab6
ngarofad <annasrofai@gmail.com>
2020-10-22 16:16:23 +0700

    add modal delete

--- application/views/v_detailRapatPIC.php
+++ application/views/v_detailRapatPIC.php
@@ -87,17 +87,70 @@
                     Peserta
                   </button>
                 </a>
-                <a href="<?php echo base_url('meetpic/delete').'/'.$ID_RAPAT ?>">
+                
+                
                   <button
                     type="button"
                     class="btn btn-outline-danger float-right ml-1"
+                    data-toggle="modal"
+                    data-target="#batalkanRapatModal"
                   >
                     <span class="p-2">
                       <i class="fas fa-trash-alt"></i>
                     </span>
                     Batalkan
                   </button>
-                </a>
+                
+                <!-- modal-->
+                <div
+                  class="modal fade"
+                  id="batalkanRapatModal"
+                  tabindex="-1"
+                  role="dialog"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                          <b>Batalkan Rapat</b>
+                        </h5>
+                        <button
+                          type="button"
+                          class="close"
+                          data-dismiss="modal"
+                          aria-label="Close"
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        
+                          <h6 class="mt-3 mb-5" > Apakah anda yakin ingin membatalkal rapat?</h6>
+                          <a href="<?php echo base_url('meetpic/delete').'/'.$ID_RAPAT ?>">
+                            <button
+                              type="button"
+                              class="btn btn-danger float-right ml-1"
+                            >
+                              Batalkan Rapat
+                            </button>
+                            </a>
+                            <button
+                              type="button"
+                              class="btn btn-outline-danger float-right"
+                              data-dismiss="modal"
+                            >
+                              Kembali
+                            </button>
+                          
+  
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!--modal-->
+               
                 <a href="<?php echo base_url('meetpic/update').'/'.$ID_RAPAT ?>">
                   <button
                     type="button"
@@ -107,6 +160,18 @@
                       <i class="fas fa-wrench"></i>
                     </span>
                     Ubah
+                  </button>
+                </a>
+                
+                <a href="<?php echo base_url('meetpic/').'/'.$ID_RAPAT ?>">
+                  <button
+                    type="button"
+                    class="btn btn-outline-danger float-right ml-1"
+                  >
+                    <span class="p-2">
+                      <i class="fas fa-clipboard"></i>
+                    </span>
+                    Tambah Hasil Rapat
                   </button>
                 </a>
               </div>
